10dbd04868a3197db9cf428ed95c5e9793c579b9
Extrabution <super.vanya-inchin-2002@ya.ru>
2021-02-17 15:00:03 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/DSALab4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSALab4.dir/main.cpp.obj"
+  "DSALab4.exe"
+  "DSALab4.exe.manifest"
+  "DSALab4.pdb"
+  "libDSALab4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSALab4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
